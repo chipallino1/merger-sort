0cea9c19b94c2e0196ef467848d886e3eb0b7f4e
yskarupich_ext <yahor.skarupich_ext@payback.net>
2022-03-02 07:36:00 +0100

    Tests updated.

--- src/test/java/com/jenkins/ExampleOfUnstableTest.java
+++ src/test/java/com/jenkins/ExampleOfUnstableTest.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ExampleOfUnstableTest {
+
     @Test
-    public void happyCase() {
+    public void test1() {
         Assertions.assertEquals(2, 1);
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertEquals(1, 1);
     }
 }
